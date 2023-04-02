6acaed20b2ff8663c7317f036abccbf5a9cb2acf
Beau Raines <beau.raines@gmail.com>
2023-04-02 12:25:54 -0700

    moves user tests to new user.test.js

---
+++ test/user.test.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import dayjs from 'dayjs';
+import debugClient from 'debug';
+import togglClient from '../index.js';
+
+const debug = debugClient('toggl-client-tests-user');
+
+describe.only('user', () => {
+  let client, workspace_id;
+  before(async () => {
+    if (!process.env.TOGGL_API_TOKEN) {
+      console.error('Please make sure to set the environment variable "TOGGL_API_TOKEN" before running the smoke tests');
+      process.exit(1);
+    }
+
+    client = togglClient();
+    const workspaces = await client.workspaces.list();
+    workspace_id = workspaces[0].id;
+  });
+
+  it('should get a user', async () => {
+    const user = await client.user.current();
+
+    debug(user);
+
+    expect(user).to.exist.to.be.an('object');
+    expect(user.email).to.exist;
+    expect(user).to.have.property('email');
+    expect(user).to.have.property('fullname');
+    expect(user).to.have.property('api_token');
+    expect(user).to.have.property('default_workspace_id');
+  });
+
+  it.skip('should get a new API token', async () => {
+    const newToken = await client.user.resetToken();
+    debug(newToken);
+    expect(newToken).to.exist.to.be.an('string');
+  });
+})
