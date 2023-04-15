f37818577235320dd90513d536703d60dfd30410
Beau Raines <beau.raines@gmail.com>
2023-04-15 10:41:02 -0700

    feat(project-users): Update project-users module for remaining v9 API changes
    Updated the other existing API calls, but skips the test because I don't have any other users in my workspace to test with
    
    Fixes #35

--- test/project-users.test.js
+++ test/project-users.test.js
@@ -4,10 +4,9 @@ import togglClient from '../index.js';
 
 const debug = debugClient('toggl-client-tests-project-users');
 
-describe.only('projects', () => {
+describe('projects', () => {
   let client;
   let workspace_id;
-  let project_id;
   before(async () => {
     if (!process.env.TOGGL_API_TOKEN) {
       console.error('Please make sure to set the environment variable "TOGGL_API_TOKEN" before running the smoke tests');
@@ -17,8 +16,6 @@ describe.only('projects', () => {
     client = togglClient();
     const workspaces = await client.workspaces.list();
     workspace_id = workspaces[0].id;
-    const projects = await client.projects.list(workspace_id);
-    project_id = projects[0].id;
   });
 
   // Add a delay of 1 second between each test case
@@ -26,9 +23,8 @@ describe.only('projects', () => {
     setTimeout(done, 1000);
   });
 
-  it('should get a project-users by project id', async () => {
-    // TODO get user id dynamically
-    const projectUsers = await client.projectUsers.get(403916);
+  it('should get project-users by project id', async () => {
+    const projectUsers = await client.projectUsers.get(workspace_id);
 
     debug(projectUsers);
 
@@ -40,38 +36,37 @@ describe.only('projects', () => {
     expect(projectUsers[0]).to.have.property('manager');
   });
 
-  it('should create, update and delete a project-user', async () => {
-    // const project = {
-    //   name: `test-project-${Date.now()}`,
-    //   workspace_id,
-    //   start_date: new Date().toISOString(),
-    // };
-    // debug(project);
-    // const createdProject = await client.projects.create(workspace_id, project);
-    // debug('createdProject');
-    // debug(createdProject);
-    // expect(createdProject).to.be.an('object');
-    // expect(createdProject).to.have.property('name').equal(project.name);
-    // expect(createdProject).to.have.property('color');
-    // expect(createdProject).to.have.property('is_private');
-    // expect(createdProject).to.have.property('workspace_id');
-    // const updatedProjectName = createdProject.description + '-updated';
-    // const updatedProject = await client.projects.update(workspace_id, createdProject.id, {
-    //   name: updatedProjectName,
-    //   workspace_id,
-    // });
-    // debug('updatedProject');
-    // debug(updatedProject);
-    // expect(updatedProject).to.be.an('object');
-    // expect(updatedProject).to.have.property('name').equal(updatedProjectName);
-    // expect(createdProject).to.have.property('color');
-    // expect(createdProject).to.have.property('is_private');
-    // expect(createdProject).to.have.property('workspace_id');
-    // await client.projects.delete(workspace_id, createdProject.id);
-    // const projectsList = await client.projects.list(workspace_id);
-    // debug('projectsList');
-    // debug(projectsList);
-    // expect(projectsList).to.be.an('array');
-    // expect(projectsList).to.not.include({ id: createdProject.id });
+  // Unable to test this without additional users
+  it.skip('should create, update and delete a project-user', async () => {
+    const projectUser = {
+      //user_id: ,
+      workspace_id,
+      manager: false,
+    };
+    debug(projectUser);
+    const addedProjectUser = await client.projectUsers.create(workspace_id, projectUser);
+    debug('addedProjectUser');
+    debug(addedProjectUser);
+    expect(addedProjectUser).to.be.an('object');
+    expect(addedProjectUser).to.have.property('workspace_id').equal(workspace_id);
+    expect(addedProjectUser).to.have.property('project_id');
+    expect(addedProjectUser).to.have.property('manager');
+
+    const updatedProjectUser = await client.projectUsers.update(workspace_id, projectUser.id, {
+      manager: true,
+    });
+    debug('updatedProjectUser');
+    debug(updatedProjectUser);
+    expect(addedProjectUser).to.be.an('object');
+    expect(addedProjectUser).to.have.property('workspace_id').equal(workspace_id);
+    expect(addedProjectUser).to.have.property('project_id');
+    expect(addedProjectUser).to.have.property('manager').equal(false);
+
+    await client.projectUsers.delete(workspace_id, projectUser.id);
+    const projectusers = client.projectUsers.get(workspace_id);
+    debug('projectusers');
+    debug(projectusers);
+    expect(projectusers).to.be.an('array');
+    expect(projectusers).to.not.include({ id: projectUser.id });
   });
 });
